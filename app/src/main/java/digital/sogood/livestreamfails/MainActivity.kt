8c6005228bb3ef4f44ef3982abfea386b6e796c7
Santeri Elo <me@santeri.xyz>
2018-09-07 18:15:06 +0300

    Initial commit, base project files

---
+++ app/src/main/java/digital/sogood/livestreamfails/MainActivity.kt
@@ -0,0 +1,13 @@
+package digital.sogood.livestreamfails
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
